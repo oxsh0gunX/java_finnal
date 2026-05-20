cf2319d6009186d22fe1964ddb41cd10cae39a68
paititi urko <146795260+oxsh0gunX@users.noreply.github.com>
2026-05-20 12:42:37 -0700

    Sort strings

---
+++ 3.java
@@ -0,0 +1,18 @@
+class Complex{
+    int real;
+    int image;
+    public  Complex(int r,int i){
+        real=r;
+        image=i;
+    }
+}
+
+class Main{
+    public static void main(String[] args ){
+        Complex c1=new Complex(2,3);
+        Complex c2=new Complex(2,3);
+        int realsum=c1.real+c2.real;
+        int imgsum=c1.image+c2.image;
+        System.out.println(realsum+"+"+imgsum+"i");
+    }
+}
